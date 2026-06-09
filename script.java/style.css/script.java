6d3b967613ebfa3e5d50b981238e4296630dc72e
millenavitoriapereira-maker <millena.vitoria.pereira@escola.pr.gov.br>
2026-06-09 15:31:38 -0300

    sandy

--- script.java/style.css/script.java
+++ script.java/style.css/script.java
@@ -1,92 +1,94 @@
-*{
-    margin:0;
-    padding:0;
-    box-sizing:border-box;
-    font-family: Arial, sans-serif;
+// ACCORDION
+
+const botoes =
+document.querySelectorAll(".accordion-btn");
+
+botoes.forEach(botao => {
+
+botao.addEventListener("click", () => {
+
+const conteudo =
+botao.nextElementSibling;
+
+if(conteudo.style.display === "block"){
+
+conteudo.style.display = "none";
+
+}else{
+
+conteudo.style.display = "block";
+
 }
 
-body{
-    background:#f4fff4;
-    color:#333;
-}
+});
 
-header{
-    background:linear-gradient(to right,#2e7d32,#66bb6a);
-    color:white;
-    text-align:center;
-    padding:40px 20px;
-}
+});
 
-header h1{
-    font-size:3rem;
-}
+// MODO ESCURO
 
-.banner{
-    text-align:center;
-    padding:50px 20px;
-}
+const modoTema =
+document.getElementById("modoTema");
 
-.banner h2{
-    color:#2e7d32;
-    margin-bottom:15px;
-}
+modoTema.addEventListener("click", () => {
 
-.cards{
-    display:flex;
-    justify-content:center;
-    flex-wrap:wrap;
-    gap:20px;
-    padding:30px;
-}
+document.body.classList.toggle("dark");
 
-.card{
-    background:white;
-    width:300px;
-    padding:20px;
-    border-radius:15px;
-    box-shadow:0 4px 10px rgba(0,0,0,0.1);
-    transition:0.3s;
-}
+});
 
-.card:hover{
-    transform:translateY(-8px);
-}
+// TAMANHO DA FONTE
 
-.card h3{
-    color:#2e7d32;
-    margin-bottom:10px;
-}
+let tamanhoFonte = 100;
 
-.interacao{
-    text-align:center;
-    padding:40px;
-}
+document
+.getElementById("aumentarFonte")
+.addEventListener("click", () => {
 
-button{
-    background:#2e7d32;
-    color:white;
-    border:none;
-    padding:12px 25px;
-    border-radius:8px;
-    font-size:18px;
-    cursor:pointer;
-    transition:0.3s;
-}
+tamanhoFonte += 10;
 
-button:hover{
-    background:#1b5e20;
-}
+document.body.style.fontSize =
+tamanhoFonte + "%";
 
-#contador{
-    margin-top:15px;
-    font-size:20px;
-    font-weight:bold;
-}
+});
 
-footer{
-    background:#2e7d32;
-    color:white;
-    text-align:center;
-    padding:15px;
-    margin-top:30px;
-}
+document
+.getElementById("diminuirFonte")
+.addEventListener("click", () => {
+
+tamanhoFonte -= 10;
+
+document.body.style.fontSize =
+tamanhoFonte + "%";
+
+});
+
+// LEITURA POR VOZ
+
+let fala;
+
+document
+.getElementById("lerPagina")
+.addEventListener("click", () => {
+
+speechSynthesis.cancel();
+
+const texto =
+document.getElementById("conteudo").innerText;
+
+fala =
+new SpeechSynthesisUtterance(texto);
+
+fala.lang = "pt-BR";
+
+speechSynthesis.speak(fala);
+
+});
+
+// PARAR LEITURA
+
+document
+.getElementById("pararLeitura")
+.addEventListener("click", () => {
+
+speechSynthesis.cancel();
+
+});
